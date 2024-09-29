7da2fd971286da07a76ed1c67fb663160446e08c
IndhuAmbati <23wh1a6658@bvrithyderabad.edu.in>
2024-09-29 15:19:33 +0530

    Create Exp1.java

---
+++ Exp1.java
@@ -0,0 +1,25 @@
+class Invoice {
+    String partNumber;
+    String partDescription;
+    int quantity;
+    double pricePerItem;
+
+    Invoice(String no, String des, int qua, double price) {
+        this.partNumber = no;
+        this.partDescription = des;
+        this.quantity = qua;
+        this.pricePerItem = price;
+    }
+
+     void getInvoiceAmount() {
+        System.out.println("Invoice Amount:  "+(quantity * pricePerItem));
+    }
+}
+ public class Exp1 {
+    public static void main(String[] args) {
+        Invoice invoice = new Invoice("123", "Book", 5, 12.99);
+        invoice.getInvoiceAmount();
+        
+       
+    }
+}
