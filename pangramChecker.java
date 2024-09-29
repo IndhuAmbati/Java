24a167d99bb7b612309b8984d98eebce8f4e6977
IndhuAmbati <23wh1a6658@bvrithyderabad.edu.in>
2024-09-29 16:05:43 +0530

    Create pangramChecker.java

---
+++ pangramChecker.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class PangramChecker {
+    static int isPangram(String sentence) {
+        sentence = sentence.toLowerCase();
+        boolean[] alphabet = new boolean[26];
+        int len = sentence.length();
+    for (int i = 0; i < len; i++) {
+            char c = sentence.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                alphabet[c - 'a'] = true;
+            }
+        }
+ for (int i = 0; i < 26; i++) {
+            if (!alphabet[i]) {
+                return -1;
+            }
+        }
+  return 1;
+    }
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String sentence = scanner.nextLine();
+     int result = isPangram(sentence);
+        System.out.println(result);
+    }
+}
